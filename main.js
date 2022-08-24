42699bea79c89accf0ec9a2d8e5762bac1919d75
Titus Huang <titus.the.huang@gmail.com>
2022-08-24 10:35:22 +0930

    Implemented basic div click function for MVP

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 // Due: Friday the 26th of August, 2022
 // Source: https://gist.git.generalassemb.ly/kasun/6c594180b30ee78feb3ff42547f4afea
 
+// Makes the squares "clickable"
+// Unsure if should be using buttons or divs, going to go with divs for now
+var playableSquares = document.querySelectorAll(".square");
+
+for (var i = 0; i < playableSquares.length; i++) {
+    playableSquares[i].addEventListener("click", function(event) {
+        event.target.querySelector("p").textContent = "X";
+    })
+}
